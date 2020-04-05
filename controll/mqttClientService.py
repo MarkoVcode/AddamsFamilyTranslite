29ade03078c8e70b9bf78c6cc411a53701fa232a
MarkoV <marek.kaszewski@gmail.com>
2020-04-05 13:05:06 +0100

    mqtt update

--- controll/mqttClientService.py
+++ controll/mqttClientService.py
@@ -32,7 +32,7 @@ def send_confirmation(topicPath, topic, message):
 ############
 def execute_request(item, value):
     if item in ITEMS_AD:
-        if (value >= 0 ) & (value <= 255):
+        if ((value >= 0 ) & (value <= 255)):
             print("the value is valid AD - execute")
             return True
         else:
