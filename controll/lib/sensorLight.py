41066558168b3af20de1325fe7f46bbe6a3ceb00
MarkoV <marek.kaszewski@gmail.com>
2020-04-06 18:22:04 +0100

    adding sensors lirary

--- controll/lib/sensorLight.py
+++ controll/lib/sensorLight.py
@@ -12,15 +12,15 @@ class sensorLight:
     self.spi = spidev.SpiDev()
     self.spi.open(0,0)
 
-# Function to read SPI data from MCP3008 chip
-# Channel must be an integer 0-7
+  # Function to read SPI data from MCP3008 chip
+  # Channel must be an integer 0-7
   def readChannel(self, channel):
     adc = self.spi.xfer2([1,(8+channel)<<4,0])
     data = ((adc[1]&3) << 8) + adc[2]
     return data
 
-# Function to convert data to voltage level,
-# rounded to specified number of decimal places.
+  # Function to convert data to voltage level,
+  # rounded to specified number of decimal places.
   def convertVolts(self, data, places):
     volts = (data * 3.3) / float(1023)
     volts = round(volts,places)
